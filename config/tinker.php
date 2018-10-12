0be1a2fb3bbe1f22b68cec6d2bedd3bca6ca43a9
Dries Vints <dries.vints@gmail.com>
2018-10-12 10:50:41 +0200

    Add config option for custom commands
    Together with an example, this should be enough for people to add their own commands to the Tinker shell.

--- config/tinker.php
+++ config/tinker.php
@@ -4,6 +4,21 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Custom commands
+    |--------------------------------------------------------------------------
+    |
+    | Sometimes, you might want to make certain console commands available in
+    | Tinker. This option allows you to do just that. Simply provide a list
+    | of commands that you want to expose to Tinker.
+    |
+    */
+
+    'commands' => [
+        // 'quote' => App\Console\Commands\QuoteCommand::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
     | Alias Blacklist
     |--------------------------------------------------------------------------
     |
