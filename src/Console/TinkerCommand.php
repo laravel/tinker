1e2d500585a4e546346fadd3adc6f9c1a97e15f4
Dries Vints <dries@vints.io>
2022-03-15 16:25:01 +0100

    Update TinkerCommand.php

--- src/Console/TinkerCommand.php
+++ src/Console/TinkerCommand.php
@@ -102,7 +102,7 @@ class TinkerCommand extends Command
         $config = $this->getLaravel()->make('config');
 
         foreach ($config->get('tinker.commands', []) as $command) {
-            $commands[] = $this->getLaravel()->resolve($command);
+            $commands[] = $this->getLaravel()->make($command);
         }
 
         return $commands;
