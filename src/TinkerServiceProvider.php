06b2996198e848dc83a5f42ab053e9da6d3368ce
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-15 22:20:04 +0800

    wip

--- src/TinkerServiceProvider.php
+++ src/TinkerServiceProvider.php
@@ -18,9 +18,9 @@ class TinkerServiceProvider extends ServiceProvider implements DeferrableProvide
     public function boot()
     {
         $source = realpath($raw = __DIR__.'/../config/tinker.php') ?: $raw;
-
+        
         if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
-            $this->publishes([$source => config_path('tinker.php')]);
+            $this->publishes([$source => $this->app->configPath('tinker.php')]);
         } elseif ($this->app instanceof LumenApplication) {
             $this->app->configure('tinker');
         }
