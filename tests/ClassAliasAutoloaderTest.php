8f4063c64bb5a39c2ae46d400995ce7d28a2e593
Jason McCreary <jason@pureconcepts.net>
2026-09-20 09:21:58 -0400

    Remove Mockery (#214)

--- tests/ClassAliasAutoloaderTest.php
+++ tests/ClassAliasAutoloaderTest.php
@@ -3,18 +3,23 @@
 namespace Laravel\Tinker\Tests;
 
 use Laravel\Tinker\ClassAliasAutoloader;
-use Mockery;
 use PHPUnit\Framework\TestCase;
 use Psy\Shell;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 class ClassAliasAutoloaderTest extends TestCase
 {
     protected $classmapPath;
     protected $loader;
+    protected $shell;
+    protected $output;
 
     protected function setUp(): void
     {
         $this->classmapPath = __DIR__.'/fixtures/vendor/composer/autoload_classmap.php';
+        $this->output = new BufferedOutput();
+        $this->shell = new Shell();
+        $this->shell->setOutput($this->output);
     }
 
     protected function tearDown(): void
@@ -25,57 +30,49 @@ class ClassAliasAutoloaderTest extends TestCase
     public function testCanAliasClasses()
     {
         $this->loader = ClassAliasAutoloader::register(
-            $shell = Mockery::mock(Shell::class),
+            $this->shell,
             $this->classmapPath
         );
 
-        $shell->shouldReceive('writeStdout')
-            ->with("[!] Aliasing 'Bar' to 'App\Foo\Bar' for this Tinker session.\n")
-            ->once();
-
         $this->assertTrue(class_exists('Bar'));
+        $this->assertSame("[!] Aliasing 'Bar' to 'App\Foo\Bar' for this Tinker session.\n", $this->output->fetch());
         $this->assertInstanceOf(\App\Foo\Bar::class, new \Bar);
     }
 
     public function testCanExcludeNamespacesFromAliasing()
     {
         $this->loader = ClassAliasAutoloader::register(
-            $shell = Mockery::mock(Shell::class),
+            $this->shell,
             $this->classmapPath,
             [],
             ['App\Baz']
         );
 
-        $shell->shouldNotReceive('writeStdout');
-
         $this->assertFalse(class_exists('Qux'));
+        $this->assertSame('', $this->output->fetch());
     }
 
     public function testVendorClassesAreExcluded()
     {
         $this->loader = ClassAliasAutoloader::register(
-            $shell = Mockery::mock(Shell::class),
+            $this->shell,
             $this->classmapPath
         );
 
-        $shell->shouldNotReceive('writeStdout');
-
         $this->assertFalse(class_exists('Three'));
+        $this->assertSame('', $this->output->fetch());
     }
 
     public function testVendorClassesCanBeWhitelisted()
     {
         $this->loader = ClassAliasAutoloader::register(
-            $shell = Mockery::mock(Shell::class),
+            $this->shell,
             $this->classmapPath,
             ['One\Two']
         );
 
-        $shell->shouldReceive('writeStdout')
-            ->with("[!] Aliasing 'Three' to 'One\Two\Three' for this Tinker session.\n")
-            ->once();
-
         $this->assertTrue(class_exists('Three'));
+        $this->assertSame("[!] Aliasing 'Three' to 'One\Two\Three' for this Tinker session.\n", $this->output->fetch());
         $this->assertInstanceOf(\One\Two\Three::class, new \Three);
     }
 }
